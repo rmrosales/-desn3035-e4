c81ce24b1aea14c24f5cc8521d44855921e257c8
rmrosales <romeomartinrosales@gmail.com>
2024-10-15 19:29:14 +0000

    added modules

---
+++ app/contact/contact.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "../components/header";
+import Nav from "../components/nav";
+
+export default function Page() {
+    return <>
+
+        <div className="container py-4">
+            <div className="row">
+
+                <Header />
+
+                <Nav />
+
+                <div className="col-9">
+                    <img className="d-block w-100 mb-3" src="https://images.unsplash.com/photo-1521017432531-fbd92d768814?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <h1>Contact Us</h1>
+                    <p>
+                        You can reach us at 123 1234 123.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+    </>
+}
---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/header";
+import Nav from "./components/nav";
+
+export default function Page() {
+    return <>
+
+        <div className="container py-4">
+            <div className="row">
+
+                <Header />
+
+                <Nav />
+
+                <div className="col-9">
+                    <img className="d-block w-100 mb-3" src="https://images.unsplash.com/photo-1521017432531-fbd92d768814?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <h1>Welcome</h1>
+                    <p>
+                        Explore our meticulously sourced beans, learn about our roasting process, and discover unique flavor profiles. Join us on a journey from farm to cup, and experience the true essence of coffee like never before!
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+    </>
+}
